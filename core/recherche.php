215fb4ef13a356f0055d1574190cf14bbf59e158
FMBee <33802035+FMBee@users.noreply.github.com>
2019-12-12 11:58:44 +0100

    page recherche, autocomplete

---
+++ core/recherche.php
@@ -0,0 +1,51 @@
+<?php
+
+if (isset($_GET) && !empty($_GET['action'])) $action=htmlentities($_GET['action']);
+else $action="readName";
+
+switch ($action) {
+	case "readName" :
+		readName();
+		break;
+	case "getNames" :
+		getNames();
+		break;
+}
+
+
+function readName() {
+	global $sql_prefix,$db,$tpldir,$tplname;
+
+	require_once ($tpldir."/forms/stagiaireseek.phtml");
+}
+
+function getNames() {
+
+    session_start();
+
+    $cheminabsolu = str_replace(
+        '\\core',
+        '',
+        __DIR__
+        );
+    require_once ("config.inc.php");
+    require_once ("../db/db.inc.php");
+
+    $result = [
+        [
+            'value' => 'DUPONT',
+            'data' => '450',
+        ],
+        [
+            'value' => 'DURAND',
+            'data' => '488',
+        ],
+    ];
+
+    echo json_encode(
+        array( 	"query" => "Unit",
+            "suggestions" => $result )
+        );
+}
+
+?>
